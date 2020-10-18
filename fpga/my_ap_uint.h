e1ff6e3541b6473a4c871c9cf67e685d81aec8d2
David Castells-Rufas <david.castells@uab.cat>
2020-10-18 19:05:38 +0200

    get bit high

--- fpga/my_ap_uint.h
+++ fpga/my_ap_uint.h
@@ -4,12 +4,27 @@
 #ifdef FPGA_DEBUG
 	typedef unsigned int ap_uint_512[512/32]; 
 	typedef unsigned int ap_uint_512p[512/32];
+
+	typedef unsigned int ap_uint_1024[1024/32];
+	typedef unsigned int ap_uint_1024p[1024/32];
+
 	#define AP_UINT_PTR(x)	      x
 	#define AP_UINT_FROM_PTR(x)   x
+
 #else
 	#include "ihc_apint.h"
 	typedef ap_uint<512> ap_uint_512;
 	typedef ap_uint<512>* ap_uint_512p;
+
+	typedef struct 
+	{
+		ap_uint_512 w1;	// high		
+		ap_uint_512 w0;	// low
+		
+	} ap_uint_1024;
+
+	typedef ap_uint_1024* ap_uint_1024p;
+ 
 	#define AP_UINT_PTR(x)	&(x)
 	#define AP_UINT_FROM_PTR(x)	*x
 #endif
@@ -22,11 +37,31 @@ unsigned char ap_uint_512_getHighByte(ap_uint_512 v, int index);
 unsigned int ap_uint_512_getDword(ap_uint_512 v, int index);
 unsigned int ap_uint_512_pop_count(ap_uint_512 x);
 int ap_uint_512_get_bit(ap_uint_512 x, int bit);
+int ap_uint_512_get_bit_high(ap_uint_512 x, int bit);
 int ap_uint_512_isZero(ap_uint_512 v);
 void ap_uint_512_shift_right(ap_uint_512 x, int shift, ap_uint_512p r);
 
+int ap_uint_1024_get_bit(ap_uint_1024 x, int bit);
+
+#ifdef FPGA_DEBUG
+
+#define assert(x) assert_function(x, __FILE__, __LINE__)
+
+void assert_function(int condition, char* file, int line)
+{
+    if (!condition)
+	printf("ASSERTION FAILED IN %s %d\n", file, line);
+}
+
+#endif
+
+
 /**
+* Sets a byte value in the lowest n byte of the number (starting from the right / LSB)
+*
+* @param v pointer to the number
 * @param index base position in bytes
+* @param value to set
 */
 void ap_uint_512_setLowByte(ap_uint_512p v, int index, int value)
 {	
@@ -39,7 +74,8 @@ void ap_uint_512_setLowByte(ap_uint_512p v, int index, int value)
 	
 	v[wordidx] = (v[wordidx] & nmask) | (value << (byteidx*8));
 #else
-	ap_uint<512> bv = value << (8 * index);
+	ap_uint<512> avalue = value;
+	ap_uint<512> bv = avalue << (8 * index);
 	ap_uint<512> mask = (0xFF) << (8 * index);
 	ap_uint<512> nmask = ~mask;
 	
@@ -105,6 +141,9 @@ unsigned int ap_uint_512_getDword(ap_uint_512 v, int index)
 #endif
 }
 
+/**
+* Resets the variable (set to zero)
+*/
 void ap_uint_512_zero(ap_uint_512p v)
 {
 #ifdef FPGA_DEBUG
@@ -116,6 +155,23 @@ void ap_uint_512_zero(ap_uint_512p v)
 	*v = 0;
 #endif
 }
+
+/**
+* Resets the variable (set to zero)
+*/
+void ap_uint_1024_zero(ap_uint_1024p v)
+{
+#ifdef FPGA_DEBUG
+	for (int i=0; i < 1024/32; i++)
+	{
+		v[i] = 0;
+	}
+#else
+	v->w0 = 0;
+	v->w1 = 0;
+#endif
+}
+
 
 int ap_uint_512_isZero(ap_uint_512 v)
 {
@@ -141,6 +197,22 @@ void ap_uint_512_set(ap_uint_512p dst, ap_uint_512 src)
 }
 
 
+/**
+* Copies a variable to another 
+* @param destination
+* @param source
+*/
+void ap_uint_1024_set(ap_uint_1024p dst, ap_uint_1024 src)
+{
+#ifdef FPGA_DEBUG
+	for (int i=0; i < 1024/32; i++)
+		dst[i] = src[i];
+#else
+	dst->w0 = src.w0;
+	dst->w1 = src.w1;
+#endif
+}
+
 void ap_uint_512_set_bit(ap_uint_512p v, int bit, int x)
 {
 #ifdef FPGA_DEBUG
@@ -158,7 +230,8 @@ void ap_uint_512_set_bit(ap_uint_512p v, int bit, int x)
 		v[wordidx] = v[wordidx] & (~(1 << bitidx));
 	}
 #else
-	ap_uint<512> mask = 1 << bit;
+	ap_uint<512> one = 1;
+	ap_uint<512> mask = one << bit;
 	ap_uint<512> nmask = ~mask;
 	
 	if (x)
@@ -168,6 +241,72 @@ void ap_uint_512_set_bit(ap_uint_512p v, int bit, int x)
 #endif
 }
 
+void ap_uint_512_or_bit(ap_uint_512p v, int bit, int x)
+{
+#ifdef FPGA_DEBUG
+	int wordidx = bit / 32;
+	int bitidx = (bit % 32);
+	
+	if (x)
+	{
+		// set bit 
+		v[wordidx] = v[wordidx] | (1 << bitidx);
+	}
+	
+#else
+	ap_uint<512> one = 1;
+	ap_uint<512> mask = one << bit;
+	
+	if (x)
+		*v = *v | mask;
+	
+#endif
+}
+
+
+void ap_uint_1024_or_bit(ap_uint_1024p v, int bit, int x)
+{
+#ifdef FPGA_DEBUG
+	int wordidx = bit / 32;
+	int bitidx = (bit % 32);
+	
+	if (x)
+	{
+		// set bit 
+		v[wordidx] = v[wordidx] | (1 << bitidx);
+	}
+#else
+	if (bit < 512)
+		ap_uint_512_or_bit(&v->w0, bit, x);
+	else
+		ap_uint_512_or_bit(&v->w1, bit-512, x);
+	
+#endif
+}
+
+void ap_uint_1024_set_bit(ap_uint_1024p v, int bit, int x)
+{
+#ifdef FPGA_DEBUG
+	int wordidx = bit / 32;
+	int bitidx = (bit % 32);
+	
+	if (x)
+	{
+		// set bit 
+		v[wordidx] = v[wordidx] | (1 << bitidx);
+	}
+	else
+	{
+		// reset bit
+		v[wordidx] = v[wordidx] & (~(1 << bitidx));
+	}
+#else
+	if (bit < 512)
+		ap_uint_512_set_bit(&v->w0, bit, x);
+	else
+		ap_uint_512_set_bit(&v->w1, bit-512, x);
+#endif
+}
 
 void ap_uint_512_onesLow(ap_uint_512p v, int n)
 {
@@ -196,6 +335,29 @@ int ap_uint_512_get_bit(ap_uint_512 x, int bit)
 	return (x[wordidx] >> bitidx) & 0x1;
 #else
 	return (x >> bit) & 0x1;
+#endif
+}
+
+int ap_uint_512_get_bit_high(ap_uint_512 x, int bit)
+{
+	return ap_uint_512_get_bit(x, 512-1-bit);
+}
+
+/**
+* return the value of a bit
+*/
+int ap_uint_1024_get_bit(ap_uint_1024 x, int bit)
+{
+#ifdef FPGA_DEBUG
+	int wordidx = bit / 32;
+	int bitidx = (bit % 32);
+	
+	return (x[wordidx] >> bitidx) & 0x1;
+#else
+	if (bit < 512)
+		return ap_uint_512_get_bit(x.w0, bit);
+	else
+		return ap_uint_512_get_bit(x.w1, bit-512);
 #endif
 }
 
@@ -254,6 +416,28 @@ void ap_uint_512_or(ap_uint_512 a, ap_uint_512 b, ap_uint_512p ret)
 	*ret = a | b;
 #endif
 }
+
+void ap_uint_1024_or(ap_uint_1024 a, ap_uint_1024 b, ap_uint_1024p ret)
+{
+#ifdef FPGA_DEBUG
+	for (int i=0; i < 1024/32; i++)
+	{
+		ret[i] = a[i]|b[i];
+	}
+#else
+	ret->w0 = a.w0 | b.w0;
+	ret->w1 = a.w1 | b.w1;
+#endif
+}
+
+void ap_uint_1024_or_self(ap_uint_1024p a, ap_uint_1024 b)
+{
+	ap_uint_1024 aux;
+	ap_uint_1024_set(AP_UINT_PTR(aux), AP_UINT_FROM_PTR(a));
+	ap_uint_1024_or(aux, b, a);
+}
+
+
 
 void ap_uint_512_xor(ap_uint_512 a, ap_uint_512 b, ap_uint_512p ret)
 {
@@ -320,14 +504,27 @@ void ap_uint_512_shift_left_self(int shift, ap_uint_512p r)
 	ap_uint_512_shift_left(aux, shift, r);
 }
 
+
+#ifdef FPGA_DEBUG	
 void ap_uint_512_print(ap_uint_512 r)
 {
 	printf("0x");
-	
+
 	for (int i=0; i < 512/32; i++)
 		printf("%08X ", ap_uint_512_getDword(r, (512/32)-1-i));
-	
 }
+#else
+void ap_uint_512_print(ap_uint<512> r)
+{
+	printf("0x");
+
+	for (int i=512-8; i >=0; i -= 8)
+	{
+		unsigned int v = (r >> i) & 0xFF;
+		printf("%02X", v);
+	}	
+}
+#endif
 
 void ap_uint_512_printBinHigh(ap_uint_512 r, int len)
 {
@@ -336,6 +533,28 @@ void ap_uint_512_printBinHigh(ap_uint_512 r, int len)
 		int bit = ap_uint_512_get_bit(r, 512-1-i);
 		printf("%d", bit);
 	}
+}
+
+void ap_uint_1024_printBinHigh(ap_uint_1024 r, int len)
+{
+	for (int i=0; i < len; i++)
+	{
+		int bit = ap_uint_1024_get_bit(r, 1024-1-i);
+		printf("%d", bit);
+	}
+}
+
+/**
+* Decode a value
+*/
+void ap_uint_1024_decode(unsigned int v, int bits, ap_uint_1024p ret)
+{
+#ifdef FPGA_DEBUG
+	assert(bits == 10);
+#endif
+	ap_uint_1024_zero(ret);
+	ap_uint_1024_or_bit(ret, v, 1);
+
 }
 
 //#ifdef FPGA_DEBUG
