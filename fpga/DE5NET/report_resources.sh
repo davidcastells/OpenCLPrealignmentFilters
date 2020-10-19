d14825bed4967fed89a2efd4de43f8acd22bc8dc
David Castells-Rufas <david.castells@uab.cat>
2020-10-19 16:43:45 +0200

    resources

--- fpga/DE5NET/report_resources.sh
+++ fpga/DE5NET/report_resources.sh
@@ -1,9 +1,19 @@
-echo "1) KERNEL SHD_3"
+echo "1) KERNEL SHD_3_VAR"
 echo "========================================================"
-grep kernel_fmax shd_3/quartus_sh_compile.log
-head -n 68 shd_3/top.flow.rpt | tail -n 30
+grep kernel_fmax shd_3_var/quartus_sh_compile.log
+head -n 68 shd_3_var/top.flow.rpt | tail -n 30
 
-echo "2) KERNEL KMERS"
+echo "2) KERNEL SHD_3_100_100"
 echo "========================================================"
-grep kernel_fmax kmers/quartus_sh_compile.log
-head -n 68 kmers/top.flow.rpt | tail -n 30
+grep kernel_fmax shd_3_100_100/quartus_sh_compile.log
+head -n 68 shd_3_100_100/top.flow.rpt | tail -n 30
+
+echo "3) KERNEL KMERS_VAR"
+echo "========================================================"
+grep kernel_fmax kmers_var/quartus_sh_compile.log
+head -n 68 kmers_var/top.flow.rpt | tail -n 30
+
+echo "4) KERNEL KMERS_100_100"
+echo "========================================================"
+grep kernel_fmax kmers_100_100/quartus_sh_compile.log
+head -n 68 kmers_100_100/top.flow.rpt | tail -n 30
