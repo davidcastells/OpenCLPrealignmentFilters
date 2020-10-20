4c474607234d96aefffdb520d81de029167d9901
David Castells-Rufas <david.castells@uab.es>
2020-10-20 06:23:44 +0200

    Update report_resources.sh
    all kernels

--- fpga/PAC10/report_resources.sh
+++ fpga/PAC10/report_resources.sh
@@ -1,9 +1,19 @@
-echo "1) KERNEL SHD_3_100_100"
+echo "1) KERNEL SHD_3_VAR"
+echo "========================================================"
+grep "Kernel fmax" shd_3_var/build/acl_quartus_report.txt
+head -n 90 shd_3_var/build/output_files/afu_fit.flow.rpt | tail -n 22
+
+echo "2) KERNEL SHD_3_100_100"
 echo "========================================================"
 grep "Kernel fmax" shd_3_100_100/build/acl_quartus_report.txt
 head -n 90 shd_3_100_100/build/output_files/afu_fit.flow.rpt | tail -n 22
 
-echo "2) KERNEL KMERS"
+echo "3) KERNEL KMERS_VAR"
 echo "========================================================"
-grep "Kernel fmax" kmers/build/acl_quartus_report.txt
-head -n 90 kmers/build/output_files/afu_fit.flow.rpt | tail -n 22
+grep "Kernel fmax" kmers_var/quartus_sh_compile.log
+head -n 90 kmers_var/build/output_files/afu_fit.flow.rpt | tail -n 22
+
+echo "4) KERNEL KMERS_100_100"
+echo "========================================================"
+grep "Kernel fmax" kmers_100_100/quartus_sh_compile.log
+head -n 90 kmers_100_100/build/output_files/afu_fit.flow.rpt | tail -n 22
