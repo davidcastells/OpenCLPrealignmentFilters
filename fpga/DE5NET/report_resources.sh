6f41bcaa74edaf2a7377e7c1bafe8cbece9aba33
David Castells-Rufas <david.castells@uab.cat>
2020-11-05 16:47:21 +0100

    shouji_3_var

--- fpga/DE5NET/report_resources.sh
+++ fpga/DE5NET/report_resources.sh
@@ -17,3 +17,8 @@ echo "4) KERNEL KMERS_100_100"
 echo "========================================================"
 grep kernel_fmax kmers_100_100/quartus_sh_compile.log
 head -n 68 kmers_100_100/top.flow.rpt | tail -n 30
+
+echo "4) KERNEL SHOUJI_3_VAR"
+echo "========================================================"
+grep kernel_fmax shouji_3_var/quartus_sh_compile.log
+head -n 68 shouji_3_var/top.flow.rpt | tail -n 30
