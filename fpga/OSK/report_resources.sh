b80901fb4fbb73fcd9278b49e6fd70d9dcca3dfd
David Castells-Rufas <david.castells@uab.cat>
2020-11-19 18:24:43 +0100

    Sneaky Snake

--- fpga/OSK/report_resources.sh
+++ fpga/OSK/report_resources.sh
@@ -17,3 +17,19 @@ echo "4) KERNEL KMERS_100_100"
 echo "========================================================"
 grep kernel_fmax kmers_100_100/quartus_sh_compile.log
 head -n 63 kmers_100_100/top.flow.rpt | tail -n 25
+
+echo "5) KERNEL SHOUJI_3_VAR"
+echo "========================================================"
+grep kernel_fmax shouji_3_var/quartus_sh_compile.log
+head -n 63 shouji_3_var/top.flow.rpt | tail -n 25
+
+echo "6) KERNEL SHOUJI_3_100_100"
+echo "========================================================"
+grep kernel_fmax shouji_3_100_100/quartus_sh_compile.log
+head -n 63 shouji_3_100_100/top.flow.rpt | tail -n 25
+
+echo "7) KERNEL SNEAKY_3_VAR"
+echo "========================================================"
+grep kernel_fmax sneaky_3_var/quartus_sh_compile.log
+head -n 63 sneaky_3_var/top.flow.rpt | tail -n 25
+
