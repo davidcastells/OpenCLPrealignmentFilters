117e7f0e6ee5273cc369312050452e9a319a62fc
David Castells-Rufas <david.castells@uab.cat>
2020-12-01 18:52:07 +0100

    sneaky

--- fpga/HARP/report_resources.sh
+++ fpga/HARP/report_resources.sh
@@ -17,3 +17,23 @@ echo "4) KERNEL KMERS_100_100"
 echo "========================================================"
 grep "Kernel fmax"  kmers_100_100/acl_quartus_report.txt
 head -n 61 kmers_100_100/top.flow.rpt | tail -n 22
+
+echo "5) KERNEL SHOUJI_3_VAR"
+echo "========================================================"
+grep "Kernel fmax"  shouji_3_var/acl_quartus_report.txt
+head -n 61 shouji_3_var/top.flow.rpt | tail -n 22
+
+echo "6) KERNEL SHOUJI_3_100_100"
+echo "========================================================"
+grep "Kernel fmax" shouji_3_100_100/acl_quartus_report.txt
+head -n 61 shouji_3_100_100/top.flow.rpt | tail -n 22
+
+echo "7) KERNEL SNEAKY_3_VAR"
+echo "========================================================"
+grep "Kernel fmax"  sneaky_3_var/acl_quartus_report.txt
+head -n 61 sneaky_3_var/top.flow.rpt | tail -n 22
+
+echo "8) KERNEL SNEAKY_3_100_100"
+echo "========================================================"
+grep "Kernel fmax" sneaky_3_100_100/acl_quartus_report.txt
+head -n 61 sneaky_3_100_100/top.flow.rpt | tail -n 22
