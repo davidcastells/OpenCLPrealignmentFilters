9d4b81b8dcd983240625ca10ea948924fad8e04e
David Castells-Rufas <david.castells@uab.cat>
2020-12-01 19:01:26 +0100

    sneaky

--- fpga/DE5NET/report_resources.sh
+++ fpga/DE5NET/report_resources.sh
@@ -15,10 +15,20 @@ head -n 68 kmers_var/top.flow.rpt | tail -n 30
 
 echo "4) KERNEL KMERS_100_100"
 echo "========================================================"
-grep kernel_fmax kmers_100_100/quartus_sh_compile.log
+grep kernel_fmax kmers_3_100_100/quartus_sh_compile.log
 head -n 68 kmers_100_100/top.flow.rpt | tail -n 30
 
-echo "4) KERNEL SHOUJI_3_VAR"
+echo "5) KERNEL SHOUJI_3_VAR"
 echo "========================================================"
 grep kernel_fmax shouji_3_var/quartus_sh_compile.log
 head -n 68 shouji_3_var/top.flow.rpt | tail -n 30
+
+echo "6) KERNEL SNEAKY_100_100"
+echo "========================================================"
+grep kernel_fmax sneaky_100_100/quartus_sh_compile.log
+head -n 68 sneaky_100_100/top.flow.rpt | tail -n 30
+
+echo "7) KERNEL SNEAKY_3_VAR"
+echo "========================================================"
+grep kernel_fmax sneaky_3_var/quartus_sh_compile.log
+head -n 68 sneaky_3_var/top.flow.rpt | tail -n 30
