b159dde66e477f0f54f1f3076398ca4fd777d45f
David Castells-Rufas <david.castells@uab.cat>
2020-12-15 18:37:44 +0100

    python make

---
+++ fpga/make_board.py
@@ -0,0 +1,46 @@
+import os
+
+
+def modified_date(path_to_file):
+    stat = os.stat(path_to_file)
+    return stat.st_mtime
+
+def withoutExtension(file):
+    part = file.split('.')
+    return part[0]
+
+def isCompiling(aocx, cl):
+    stream = os.popen('ps -ef')
+    outStr = stream.read()
+    return (aocx in outStr)
+
+def isCompiled(aocx, cl):
+
+    if (os.path.exists(aocx) == False):
+        print(aocx + ' is not compiled')
+        return False
+
+    aocx_date = modified_date(aocx)
+    cl_date = modified_date(cl)
+    if (aocx_date > cl_date):
+        return True
+
+def makeAocx(aocx, cl, threshold, pattern_len, text_len):
+    if (isCompiling(aocx, cl)):
+        print('Already compiling ' + aocx)
+        return
+    if (isCompiled(aocx, cl)):
+        print('Already compiled ' + aocx)
+        return
+
+    common_flags = '-D BASIC_AP_UINT -I $INTELFPGAOCLSDKROOT/include/kernel_headers -g'
+    design_name = withoutExtension(aocx)
+    threshold_flag = withoutExtension(aocx) + '_THRESHOLD'
+
+    item_flags = ' -D {}={} -D PATTERN_LEN={} -D TEXT_LEN={} '.format(threshold_flag, threshold, pattern_len, text_len)
+    nohup = 'nohup'
+    outlog = ' >> compile.' + design_name + '.out'
+    cmd = nohup + ' aoc '+common_flags+item_flags+cl+' -o '+aocx + outlog + '&'
+    print(cmd)
+    os.system(cmd)
+    
