6f0bb91d50c944c5562a700d83daea991a5d45ec
David Castells-Rufas <david.castells@uab.cat>
2020-12-21 08:33:42 +0100

    improved control of tests

--- test/test_accuracy_SW.py
+++ test/test_accuracy_SW.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 
-def test(name, EI, ED, ES, th, N):
-	cmd = '../filter-{} -ps 0 -EI {} -ED {} -ES {} -th {} -pl 100 -tl 100 -n {}'.format(name, EI, ED, ES, th, N)
+def test(name, EI, ED, ES, th, PL, TL, N):
+	cmd = '../filter-{} -ps 0 -EI {} -ED {} -ES {} -th {} -pl {} -tl {} -n {}'.format(name, EI, ED, ES, th, PL, TL, N)
 	print(cmd)
 	stream = os.popen(cmd)
 	outStr = stream.read()
@@ -25,48 +25,57 @@ def test(name, EI, ED, ES, th, N):
 	return (EI, ED, ES, th, accuracyLine, FP, FN, Total)
 
 
-def testRangeErrors(gdf, name):
+def testRangeErrors(gdf, name, th, baselen):
 	N = 1000
 	ES = 0
+	EMAX = 40
+	PL = baselen
+	TL = baselen
 
-	acur = { 'Alg':[], 'EI':[], 'ED':[], 'ES':[], 'th':[], 'N':[], 'FP':[], 'FN':[], 'Total':[]}
+	acur = { 'Alg':[], 'Test':[], 'EI':[], 'ED':[], 'ES':[], 'th':[], 'PL':[], 'TL':[], 'N':[], 'FP':[], 'FN':[], 'Total':[]}
 
-	for EI in range(16):
+	for EI in range(EMAX):
 		ED = EI
-		tuple = test(name, EI,ED,ES, th, N)
+		tuple = test(name, EI,ED,ES, th, PL, TL, N)
 		acur['Alg'].append(name)
+		acur['Test'].append('I')		
 		acur['EI'].append(EI)
 		acur['ED'].append(ED)
 		acur['ES'].append(ES)
 		acur['th'].append(th)
+		acur['PL'].append(PL)
+		acur['TL'].append(TL)
 		acur['N'].append(N)		
 		acur['FP'].append(tuple[5])
 		acur['FN'].append(tuple[6])
 		acur['Total'].append(tuple[7])
 		
-	df = pd.DataFrame(acur, columns=['Alg', 'EI','ED','ES','th','N','FP','FN','Total'])
+	df = pd.DataFrame(acur, columns=['Alg', 'Test', 'EI','ED','ES','th', 'PL', 'TL', 'N','FP','FN','Total'])
 	print('{} Insertion/Deletion Errors'.format(name))
 	print(df)
 
 	gdf = gdf.append(df);
 
-	acur = { 'Alg':[], 'EI':[], 'ED':[], 'ES':[], 'th':[], 'N':[], 'FP':[], 'FN':[], 'Total':[]}
+	acur = { 'Alg':[], 'Test':[], 'EI':[], 'ED':[], 'ES':[], 'th':[], 'PL':[], 'TL':[], 'N':[], 'FP':[], 'FN':[], 'Total':[]}
 
 	EI = 0
 	ED = 0
-	for ES in range(16):
-		tuple = test(name, EI,ED,ES, th, N)
+	for ES in range(EMAX):
+		tuple = test(name, EI,ED,ES, th, PL, TL, N)
 		acur['Alg'].append(name)
+		acur['Test'].append('S')
 		acur['EI'].append(EI)
 		acur['ED'].append(ED)
 		acur['ES'].append(ES)
 		acur['th'].append(th)
+		acur['PL'].append(PL)
+		acur['TL'].append(TL)
 		acur['N'].append(N)		
 		acur['FP'].append(tuple[5])
 		acur['FN'].append(tuple[6])
 		acur['Total'].append(tuple[7])
 		
-	df = pd.DataFrame(acur, columns=['Alg','EI','ED','ES','th','N','FP','FN','Total'])
+	df = pd.DataFrame(acur, columns=['Alg', 'Test', 'EI','ED','ES','th','PL', 'TL', 'N','FP','FN','Total'])
 	print('{} Substitution Errors'.format(name))
 	print(df)
 
@@ -74,14 +83,21 @@ def testRangeErrors(gdf, name):
 	return gdf
 
 
-th = 3
+ths = [[3, 5, 7], [3,7,11], [5,10,15]]
+baselens = [100, 150, 300]
 
-acur = { 'Alg':[], 'EI':[], 'ED':[], 'ES':[], 'th':[], 'N':[], 'FP':[], 'FN':[], 'Total':[]}
-df = pd.DataFrame(acur, columns=['Alg', 'EI','ED','ES','th','N','FP','FN','Total'])
 
-df = testRangeErrors(df, 'shd')
-df = testRangeErrors(df, 'shouji')
-df = testRangeErrors(df, 'sneaky')
-df = testRangeErrors(df, 'kmers')
+for baselenidx in range(3):
+	acur = { 'Alg':[], 'Test':[], 'EI':[], 'ED':[], 'ES':[], 'th':[], 'PL':[], 'TL':[], 'N':[], 'FP':[], 'FN':[], 'Total':[]}
+	df = pd.DataFrame(acur, columns=['Alg', 'Test', 'EI','ED','ES','th','PL', 'TL', 'N','FP','FN','Total'])
 
-df.to_csv('results_SW.csv');
+	baselen = baselens[baselenidx]
+	ths_len = ths[baselenidx] 
+	for th in ths_len:
+		df = testRangeErrors(df, 'shd', th, baselen)
+		df = testRangeErrors(df, 'shouji', th, baselen)
+		df = testRangeErrors(df, 'shoujialser', th, baselen)
+		df = testRangeErrors(df, 'sneaky', th, baselen)
+		df = testRangeErrors(df, 'kmers', th, baselen)
+
+	df.to_csv('results_SW_{}.csv'.format(baselen));
