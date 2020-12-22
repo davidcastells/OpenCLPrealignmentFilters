acc8498e359150ad51d300cb5f3f37679e4cb654
David Castells-Rufas <david.castells@uab.cat>
2020-12-22 08:46:18 +0100

    support for design specification

--- fpga/DE5NET/report_resources.py
+++ fpga/DE5NET/report_resources.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def reportDesign(name, th, type):
 	print('Results for', name, th, type, '----------------------')
@@ -9,16 +10,30 @@ def reportDesign(name, th, type):
 	os.system('grep kernel_fmax {}/quartus_sh_compile.log'.format(dir))
 	os.system('head -n 68 {}/top.flow.rpt | tail -n 30'.format(dir))
 
-for th in [3,5,7]:
-	#reportDesign('shd', th, 'var')
-	reportDesign('shd', th, '100_100')
-	#reportDesign('shouji', th, 'var')
-	reportDesign('shouji', th, '100_100')
-	#reportDesign('sneaky', th, 'var')
-	reportDesign('sneaky', th, '100_100')
+
+if __name__ == "__main__":
+
+	design = ['shd', 'shouji', 'sneaky', 'kmers']
+	thresholdbased = ['shd', 'shouji', 'sneaky']
+		
+	print(f"Arguments count: {len(sys.argv)}")
+	for i, arg in enumerate(sys.argv):
+		if (arg.startswith('design=all')):
+			design = ['shd', 'shouji', 'sneaky', 'kmers']
+		elif (arg.startswith('design=')):
+			part = arg.split('=')
+			design = part[1].split(',')
+
+	for des in design:	
+		if (des in thresholdbased):
+			for th in [3,5,7]:
+				#reportDesign(des, th, 'var')
+				reportDesign(des, th, '100_100')
+				
 	
-reportDesign('kmers', -1, '100_100')
-reportDesign('kmers', -1, 'var')
+	if 'kmer' in design:
+		reportDesign('kmers', -1, '100_100')
+		reportDesign('kmers', -1, 'var')
 
 
 
