cf8f5c13636f99804af2954bd39cd7a24c8d8a29
David Castells-Rufas <david.castells@uab.cat>
2020-12-24 00:39:50 +0100

    specify variants to make

--- fpga/OSK/make.py
+++ fpga/OSK/make.py
@@ -6,5 +6,5 @@ import sys
 sys.path.append('..')
 import make_board as mb
 
-mb.makeVariants(BOARD, AOCL_FLAGS)
+mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[3,7,10]], lens = [150], entry_types = [1])
 
