ea102f844695a38237a9b5e49ace076c3a82875a
David Castells-Rufas <david.castells@uab.cat>
2020-12-23 20:54:33 +0100

    blocking

--- fpga/make_board.py
+++ fpga/make_board.py
@@ -1,5 +1,10 @@
 import os
 
+gCompiled = 0
+gCompiling = 0
+gStarted = 0
+
+gOnFlyMax = 4
 
 def modified_date(path_to_file):
     stat = os.stat(path_to_file)
@@ -30,11 +35,21 @@ def designConstant(aocx):
     return part[0].upper()
 
 def makeAocx(aocx, cl, threshold=-1, pattern_len=-1, text_len=-1, entry_type=0, extra_flags='', blocking=False):
+    global gCompiling
+    global gCompiled
+    global gStarted
+
     if (isCompiling(aocx, cl)):
         print('Already compiling ' + aocx)
+        gCompiling = gCompiling + 1
         return
     if (isCompiled(aocx, cl)):
         print('Already compiled ' + aocx)
+        gCompiled = gCompiled + 1
+        return
+
+    if ((gCompiling + gStarted) > gOnFlyMax and blocking == False):
+        print('concurrent works limit reached')
         return
 
     entry_type_flags = ' -D ENTRY_TYPE_{} '.format(entry_type)
@@ -63,23 +78,27 @@ def makeAocx(aocx, cl, threshold=-1, pattern_len=-1, text_len=-1, entry_type=0, 
     os.system('echo "'+cmd+'" > compile.' + design_name + '.out')
     os.system(cmd)
 
-def makeVariants(BOARD, AOCL_FLAGS):
-	ths = [[3,5,7],[3,7,10]]
-	lens = [100,150]
-	entry_types = [0,1]
+    gStarted = gStarted + 1
+
+
+
+def makeVariants(BOARD, AOCL_FLAGS, ths= [[3,5,7],[3,7,10]], lens = [100,150], entry_types = [0,1], blocking=False):
+	
 
 	for i in range(len(lens)):
 		plen = lens[i]
 		et = entry_types[i]
 
 		for th in ths[i]:
-			makeAocx(aocx='shd_e{}_{}_{}_{}.aocx'.format(et,th, plen, plen), cl='../shd.cl', threshold=th, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et)
-			makeAocx(aocx='shouji_e{}_{}_{}_{}.aocx'.format(et,th, plen, plen), cl='../shouji.cl', threshold=th, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et)
-			makeAocx(aocx='sneaky_e{}_{}_{}_{}.aocx'.format(et,th, plen, plen), cl='../sneaky.cl', threshold=th, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et)
+			makeAocx(aocx='shd_e{}_{}_{}_{}.aocx'.format(et,th, plen, plen), cl='../shd.cl', threshold=th, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et, blocking=blocking)
+			makeAocx(aocx='shouji_e{}_{}_{}_{}.aocx'.format(et,th, plen, plen), cl='../shouji.cl', threshold=th, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et, blocking=blocking)
+			makeAocx(aocx='sneaky_e{}_{}_{}_{}.aocx'.format(et,th, plen, plen), cl='../sneaky.cl', threshold=th, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et, blocking=blocking)
 
 
-		makeAocx(aocx='kmers_e{}_{}_{}.aocx'.format(et,plen,plen), cl='../kmers.cl', threshold=-1, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et)
-		makeAocx(aocx='kmers_e{}_var.aocx'.format(et), cl='../kmers.cl', extra_flags=AOCL_FLAGS, entry_type=et)
+		makeAocx(aocx='kmers_e{}_{}_{}.aocx'.format(et,plen,plen), cl='../kmers.cl', threshold=-1, pattern_len=plen, text_len=plen, extra_flags=AOCL_FLAGS, entry_type=et, blocking=blocking)
+		makeAocx(aocx='kmers_e{}_var.aocx'.format(et), cl='../kmers.cl', extra_flags=AOCL_FLAGS, entry_type=et, blocking=blocking)
     
+
+	print('SUMMARY: Compiled:' , gCompiled, 'Compiling:', gCompiling, 'Started', gStarted);
 
 
