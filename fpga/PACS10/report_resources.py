d989113908ab14bdf26fe08100a9c675af4cda98
David Castells-Rufas <david.castells@uab.cat>
2020-12-27 05:04:34 -0800

    python based

---
+++ fpga/PACS10/report_resources.py
@@ -0,0 +1,27 @@
+import os
+import sys
+sys.path.append('..')
+import report_board as rb
+
+
+if __name__ == "__main__":
+    doTable = False
+
+    design = ['shd', 'shouji', 'sneaky', 'kmers']
+    for i, arg in enumerate(sys.argv):
+        if (arg.startswith('design=all')):
+            design = ['shd', 'shouji', 'sneaky', 'kmers']
+        elif (arg.startswith('design=')):
+            part = arg.split('=')
+            design = part[1].split(',')
+
+        if (arg.startswith('table')):
+            doTable = True;
+
+    if (doTable):
+        rb.reportTable('PACS10')
+    else:
+        rb.reportDesigns('PACS10',design)
+
+
+
