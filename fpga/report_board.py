f90c747221537fba1a397718202be95afb874caa
David Castells-Rufas <david.castells@uab.cat>
2020-12-31 02:41:07 -0800

    PAC boards

--- fpga/report_board.py
+++ fpga/report_board.py
@@ -1,37 +1,96 @@
 import os
 import sys
 
-def reportDesign(board, name, th, et, pl):
+def systemOutput(cmd):
+    stream = os.popen(cmd)
+    outStr = stream.read()
+    lines = outStr.split('\n');
+    return lines
 
-	info = {
+def getInfo():
+    info = {
 		'DE5NET': {'fmax':'kernel_fmax', 'fmax-file':'quartus_sh_compile.log', 'resources-file':'top.flow.rpt', 'start':68, 'len':30},
 		'OSK' :   {'fmax':'kernel_fmax', 'fmax-file':'quartus_sh_compile.log', 'resources-file':'top.flow.rpt', 'start':63, 'len':25},
-	}
+		'HARP' :   {'fmax':'kernel_fmax', 'fmax-file':'quartus_sh_compile.log', 'resources-file':'top.flow.rpt', 'start':63, 'len':25},
+		'PAC10' :   {'fmax':'"Kernel fmax"', 'fmax-file':'acl_quartus_report.txt', 'resources-file':'acl_quartus_report.txt', 'start':7, 'len':7},
+		'PACS10' :   {'fmax':'"Kernel fmax"', 'fmax-file':'build/acl_quartus_report.txt', 'resources-file':'build/output_files/afu_default.flow.rpt', 'start':75, 'len':22},
+    }
+    return info
 
-	if (th == -1):
-		dir = '{}_e{}'.format(name, et)
-	else:
-		dir = '{}_e{}_{}'.format(name, et, th)
+def getDesignDir(dsg, et, th, pl):
+    if (th == -1):
+        dir = '{}_e{}'.format(dsg, et)
+    else:
+        dir = '{}_e{}_{}'.format(dsg, et, th)
+    if (pl == -1):
+        ptype = '_var'
+    else:
+        ptype = '_{}_{}'.format(pl, pl)
+    return dir+ptype
 
-	if (pl == -1):
-		ptype = '_var'
-	else:
-		ptype =  '_{}_{}'.format(pl, pl)
+def getDesignFmax(board, dsg, et, th, pl):
+    info = getInfo()
+    sfmaxfile = info[board]['fmax-file']
+    query = info[board]['fmax']
+    sdir = getDesignDir(dsg, et, th, pl)
+    sfile = '{}/{}'.format(sdir, sfmaxfile)
+    if (os.path.exists(sfile) == False):
+        return '?'
+    cmd = 'grep {} {}'.format(query, sfile)
+    sout = systemOutput(cmd)
+    return int(float(sout[0].split(':')[1].strip()))
 
-	aocx = dir+ptype
+def getDesignResources(board, dsg, et, th, pl):
+    info = getInfo()
+    sresourcesfile = info[board]['resources-file']
+    sdir = getDesignDir(dsg, et, th, pl)
+    sfile = '{}/{}'.format(sdir, sresourcesfile)
+    if (os.path.exists(sfile) == False):
+        return '?'
+    cmd = ' grep "Logic utilization" {}'.format(sfile)
+    sout = systemOutput(cmd)
+    ret = sout[0]
+    if ('ALMs' in ret):
+        # PACS10
+        part = ret.split('(')
+        part = part[2].split(')')
+        ret = part[0]
+        return ret.strip()
+    elif ('Logic utilization' in ret):
+        part = ret.split('(')
+        part = part[1].split(')')
+        ret = part[0].strip()
+    else:
+        return '?'
+    return ret
 
-	print("", flush=True)	
-	print("========================================================", flush=True)
-	print('| Results for', aocx,  flush=True)
-	print("========================================================", flush=True)
+def reportDesign(board, name, th, et, pl):
+    info = getInfo()
+
+    if (th == -1):
+        dir = '{}_e{}'.format(name, et)
+    else:
+        dir = '{}_e{}_{}'.format(name, et, th)
+
+    if (pl == -1):
+        ptype = '_var'
+    else:
+        ptype =  '_{}_{}'.format(pl, pl)
+
+    aocx = dir+ptype
+
+    print("", flush=True)	
+    print("========================================================", flush=True)
+    print('| Results for', aocx,  flush=True)
+    print("========================================================", flush=True)
 	
-	sfmax = info[board]['fmax']
-	sfmaxfile = info[board]['fmax-file']
-	sstart = info[board]['start']
-	slen = info[board]['len']
-	sresourcesfile = info[board]['resources-file']
-	os.system('grep {} {}/{}'.format(sfmax, aocx, sfmaxfile))
-	os.system('head -n {} {}/{} | tail -n {}'.format(sstart, aocx, sresourcesfile, slen))
+    sfmax = info[board]['fmax']
+    sfmaxfile = info[board]['fmax-file']
+    sstart = info[board]['start']
+    slen = info[board]['len']
+    sresourcesfile = info[board]['resources-file']
+    os.system('grep {} {}/{}'.format(sfmax, aocx, sfmaxfile))
+    os.system('head -n {} {}/{} | tail -n {}'.format(sstart, aocx, sresourcesfile, slen))
 
 
 def reportDesigns(board, design):
@@ -56,3 +115,52 @@ def reportDesigns(board, design):
 		if 'kmer' in design:
 			reportDesign(board, 'kmers', -1, et, pl)
 			reportDesign(board, 'kmers', -1, et, -1)
+
+
+def reportTable(board):
+    ths = [[3,5,7],[3,7,10],[5,10,15]]
+    ets = [0,1,2]
+    pls = [100,150,300]
+
+    print('RESOURCE TABLE')
+    print('------------------------------------------------------------------')
+    slink = '|{: <10}|'.format('')
+    for etidx, et in enumerate(ets):
+        print('{}{: <17}|'.format(slink, et), end='')
+        slink = ''
+    print('')
+
+    slink = '|{: <10}|'.format('Design')
+    for etidx, et in enumerate(ets):
+
+        for th in ths[etidx]:
+            print('{}{: <5}|'.format(slink, th), end='')
+            slink = ''
+    print('')
+    for dsg in ['shd', 'shouji', 'sneaky']:
+        slink = '|{: <10}|'.format(dsg)
+        for etidx, et in enumerate(ets):
+            for th in ths[etidx]:
+                v = getDesignResources(board, dsg, et, th, pls[etidx])
+                print('{}{: <5}|'.format(slink, v), end='')
+                slink = ''
+        print('')
+
+    print('')
+    print('Fmax TABLE')
+    print('-------------------------------------------------------------------')
+    slink = '|{: <10}|'.format('')
+    for etidx, et in enumerate(ets):
+        print('{}{: <17}|'.format(slink, et), end='')
+        slink = ''
+    print('')
+    for dsg in ['shd', 'shouji', 'sneaky']:
+        slink = '|{: <10}|'.format(dsg)
+        for etidx, et in enumerate(ets):
+            for th in ths[etidx]:
+                v = getDesignFmax(board, dsg, et, th, pls[etidx])
+                print('{}{: <5}|'.format(slink, v), end='')
+                slink = ''
+        print('')
+
+
