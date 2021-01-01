25828066ec527cb5569fd15f97896093cd22ce51
David Castells-Rufas <david.castells@uab.cat>
2021-01-01 10:14:39 +0100

    metaprogramming

--- fpga/make_board.py
+++ fpga/make_board.py
@@ -14,7 +14,7 @@ def withoutExtension(file):
     part = file.split('.')
     return part[0]
 
-def isCompiling(aocx, cl):
+def isCompiling(aocx):
     stream = os.popen('ps -ef')
     outStr = stream.read()
     return (aocx in outStr)
@@ -38,30 +38,41 @@ def system(cmd):
     print(cmd)
     os.system(cmd)
 
-def metaprogram(dsg, level='', flags=''):
-    cl = '{}.cl'.format(dsg)
+def metaprogram(dsg,  meta=None, level='', cl=None, flags=''):
+    if (cl == None):
+       cl = '../{}.cl'.format(dsg)
     cl_generator = '/tmp/{}.cl.generator'.format(dsg)
     cl_generator_cpp = cl_generator +'.cpp'
-    meta = '{}{}.cl.metaprogram'.format(dsg, level)
+    if (meta == None):
+       meta = '../{}{}.cl.metaprogram'.format(dsg, level)
 
-    system('../metagenerator ../{} > {}'.format(meta, cl_generator_cpp))
+    if (isCompiled(cl, meta)):
+        print('Skipping metaprogramming. {} already metaprogrammed'.format(cl))
+        return
+
+    system('../metagenerator {} > {}'.format(meta, cl_generator_cpp))
     system('g++ {} {} -o {}'.format(cl_generator_cpp, flags, cl_generator))
-    system('{} > ../{}'.format(cl_generator, cl))
+    system('{} > {}'.format(cl_generator, cl))
 
-def makeAocx(aocx, cl, threshold=-1, pattern_len=-1, text_len=-1, entry_type=0, extra_flags='', blocking=False, ignoretargets=[]):
+def makeAocx(aocx, cl, threshold=-1, pattern_len=-1, text_len=-1, entry_type=0, extra_flags='', blocking=False, ignoretargets=[], meta=None):
     global gCompiling
     global gCompiled
     global gStarted
+
+    if (meta == None):
+        filedate = meta
+    else:
+        filedate = cl
 
     if (aocx in ignoretargets):
         print(aocx, 'in ignore list')
         return
 
-    if (isCompiling(aocx, cl)):
+    if (isCompiling(aocx)):
         print('Already compiling ' + aocx)
         gCompiling = gCompiling + 1
         return
-    if (isCompiled(aocx, cl)):
+    if (isCompiled(aocx, filedate)):
         print('Already compiled ' + aocx)
         gCompiled = gCompiled + 1
         return
@@ -100,7 +111,7 @@ def makeAocx(aocx, cl, threshold=-1, pattern_len=-1, text_len=-1, entry_type=0, 
 
 
 
-def makeVariants(BOARD, AOCL_FLAGS, ths= [[3,5,7],[3,7,10],[5,10,15]], lens = [100,150,300], entry_types = [0,1,2], blocking=False, ignoretargets=[]):
+def makeVariants(BOARD, AOCL_FLAGS, ths=[[3,5,7],[3,7,10],[5,10,15]], lens=[100,150,300], entry_types=[0,1,2], blocking=False, ignoretargets=[]):
    for i in range(len(lens)):
       plen = lens[i]
       et = entry_types[i]
@@ -114,4 +125,30 @@ def makeVariants(BOARD, AOCL_FLAGS, ths= [[3,5,7],[3,7,10],[5,10,15]], lens = [1
 
    print('SUMMARY: Compiled:' , gCompiled, 'Compiling:', gCompiling, 'Started', gStarted);
 
+
+def makeMetaVariants(BOARD, AOCL_FLAGS, blocking=False):
+
+   print('COMPILING SHD for {}:'.format(BOARD));
+
+   dsg = 'shd'
+   meta = '../shd.cl.metaprogram'
+   metaprogram(dsg, meta=meta, cl='shd_e2_5_300_300.cl', flags='-D ENTRY_TYPE_2 -D SHD_THRESHOLD=5')
+   metaprogram(dsg, meta=meta, cl='shd_e2_10_300_300.cl', flags='-D ENTRY_TYPE_2 -D SHD_THRESHOLD=10')
+   metaprogram(dsg, meta=meta, cl='shd_e2_15_300_300.cl', flags='-D ENTRY_TYPE_2 -D SHD_THRESHOLD=15')
+
+   makeAocx(aocx='shd_e2_5_300_300.aocx', cl='shd_e2_5_300_300.cl', threshold=5, pattern_len=300, text_len=300, extra_flags=AOCL_FLAGS, entry_type=2, blocking=blocking, meta=meta)
+   makeAocx(aocx='shd_e2_10_300_300.aocx', cl='shd_e2_10_300_300.cl', threshold=10, pattern_len=300, text_len=300, extra_flags=AOCL_FLAGS, entry_type=2, blocking=blocking, meta=meta)
+   makeAocx(aocx='shd_e2_15_300_300.aocx', cl='shd_e2_15_300_300.cl', threshold=15, pattern_len=300, text_len=300, extra_flags=AOCL_FLAGS, entry_type=2, blocking=blocking, meta=meta)
+
+   dsg = 'shouji'
+   meta = '../shouji.cl.metaprogram'
+   metaprogram(dsg, meta=meta, cl='shouji_e2_5_300_300.cl', flags='-D ENTRY_TYPE_2 -D SHOUJI_THRESHOLD=5')
+   metaprogram(dsg, meta=meta, cl='shouji_e2_10_300_300.cl', flags='-D ENTRY_TYPE_2 -D SHOUJI_THRESHOLD=10')
+   metaprogram(dsg, meta=meta, cl='shouji_e2_15_300_300.cl', flags='-D ENTRY_TYPE_2 -D SHOUJI_THRESHOLD=15')
+
+   makeAocx(aocx='shouji_e2_5_300_300.aocx', cl='shouji_e2_5_300_300.cl', threshold=5, pattern_len=300, text_len=300, extra_flags=AOCL_FLAGS, entry_type=2, blocking=blocking, meta=meta)
+   makeAocx(aocx='shouji_e2_10_300_300.aocx', cl='shouji_e2_10_300_300.cl', threshold=10, pattern_len=300, text_len=300, extra_flags=AOCL_FLAGS, entry_type=2, blocking=blocking, meta=meta)
+   makeAocx(aocx='shouji_e2_15_300_300.aocx', cl='shouji_e2_15_300_300.cl', threshold=15, pattern_len=300, text_len=300, extra_flags=AOCL_FLAGS, entry_type=2, blocking=blocking, meta=meta)
+
+   print('SUMMARY: Compiled:' , gCompiled, 'Compiling:', gCompiling, 'Started', gStarted);
 
