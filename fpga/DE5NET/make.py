eb68a2ef17602f58ced0cd669a81fa0c24890e91
David Castells-Rufas <david.castells@uab.cat>
2021-01-01 10:13:13 +0100

    meta variants

--- fpga/DE5NET/make.py
+++ fpga/DE5NET/make.py
@@ -6,5 +6,10 @@ import sys
 sys.path.append('..')
 import make_board as mb
 
-mb.makeVariants(BOARD, AOCL_FLAGS)
+blocking=False
+
+mb.makeMetaVariants(BOARD, AOCL_FLAGS, blocking=blocking)
+
+#mb.makeVariants(BOARD, AOCL_FLAGS)
 #mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[3,7,10]], lens = [150], entry_types = [1])
+#mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[15]], lens = [300], entry_types = [2])
