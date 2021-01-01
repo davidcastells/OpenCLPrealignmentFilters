c8ad9263adb553474fb3ca4324d275c62633770a
David Castells-Rufas <david.castells@uab.cat>
2021-01-01 14:01:07 +0100

    metaprogramming

--- fpga/HARP/make.py
+++ fpga/HARP/make.py
@@ -2,8 +2,16 @@ BOARD='HARP'
 TIME='/usr/bin/time -f TIME=%E'
 AOCL_FLAGS=''
 
+import time
 import sys
 sys.path.append('..')
 import make_board as mb
 
-mb.makeVariants(BOARD, AOCL_FLAGS)
+
+for th in [5]:
+	mb.metaprogram('sneaky', level='_level_2', flags=' -D SNEAKY_THRESHOLD={} -D PATTERN_LEN={}'.format(th, 8*2))
+	#mb.makeVariants(BOARD, AOCL_FLAGS)
+	mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[th]], lens = [100], entry_types = [0], blocking=False, ignoretargets=[])
+	#time.sleep(10)
+
+
