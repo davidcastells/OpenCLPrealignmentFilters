d9aa150b6a700e09ac9f6d1760ea88450ff86a6f
David Castells-Rufas <david.castells@uab.cat>
2021-01-03 08:30:28 +0100

    python scripts

--- fpga/HARP/make.py
+++ fpga/HARP/make.py
@@ -7,11 +7,15 @@ import sys
 sys.path.append('..')
 import make_board as mb
 
+blocking=False
 
-for th in [5]:
-	mb.metaprogram('sneaky', level='_level_2', flags=' -D SNEAKY_THRESHOLD={} -D PATTERN_LEN={}'.format(th, 8*2))
-	#mb.makeVariants(BOARD, AOCL_FLAGS)
-	mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[th]], lens = [100], entry_types = [0], blocking=False, ignoretargets=[])
-	#time.sleep(10)
+mb.makeMetaVariants(BOARD, AOCL_FLAGS, blocking=blocking)
+
+
+#for th in [5]:
+#   mb.metaprogram('sneaky', level='_level_2', flags=' -D SNEAKY_THRESHOLD={} -D PATTERN_LEN={}'.format(th, 8*2))
+#   mb.makeVariants(BOARD, AOCL_FLAGS)
+#   mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[th]], lens = [100], entry_types = [0], blocking=False, ignoretargets=[])
+#   #time.sleep(10)
 
 
