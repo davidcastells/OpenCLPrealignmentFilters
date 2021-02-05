8152976348469f6348e2b0081bdae04d0605ad81
David Castells-Rufas <david.castells@uab.cat>
2021-02-05 07:37:25 -0800

    lev

--- fpga/PACS10/build_stratix10.sh
+++ fpga/PACS10/build_stratix10.sh
@@ -3,3 +3,4 @@ source /data/intel_fpga/devcloudLoginToolSetup.sh
 tools_setup -t S10DS 
 cd ~/INT_Bioinformatics/OpenCLPrealignmentFilters/fpga/PACS10
 python make.py
+#aoc -v shd_e2_15_300_300.aoco -o shd_e2_15_300_300.aocx
