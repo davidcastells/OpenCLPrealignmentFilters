a692184c66091175922b453b7ce2ea69b45fe1de
David Castells-Rufas <david.castells@uab.cat>
2021-02-18 20:50:27 +0100

    myers

--- fpga/PAC10/make.py
+++ fpga/PAC10/make.py
@@ -6,9 +6,12 @@ import sys
 sys.path.append('..')
 import make_board as mb
 
-blocking=True
+#blocking=True
+blocking=False
 
-mb.makeMetaVariants(BOARD, AOCL_FLAGS, blocking=blocking)
+#mb.makeMetaVariants(BOARD, AOCL_FLAGS, blocking=blocking)
 
 #mb.makeVariants(BOARD, AOCL_FLAGS, blocking=True)
+#mb.makeLev(BOARD, AOCL_FLAGS, blocking=blocking)
+mb.makeMyers(BOARD, AOCL_FLAGS, blocking=blocking)
 
