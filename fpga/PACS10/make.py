b21adbd55ca377d83f4e42ae95a63f22a1203b80
David Castells-Rufas <david.castells@uab.cat>
2021-03-27 08:43:45 +0100

    bpc

--- fpga/PACS10/make.py
+++ fpga/PACS10/make.py
@@ -6,11 +6,14 @@ import sys
 sys.path.append('..')
 import make_board as mb
 
-blocking=True
+#blocking=True
+blocking=False
 
 #mb.makeSemiglobalVariants(BOARD, AOCL_FLAGS, blocking=blocking)
 #mb.makeMetaVariants(BOARD, AOCL_FLAGS, blocking=blocking)
 
 #mb.makeVariants(BOARD, AOCL_FLAGS, blocking=True)
 
-mb.makeLev(BOARD, AOCL_FLAGS, blocking=blocking)
+#mb.makeLev(BOARD, AOCL_FLAGS, blocking=blocking)
+#mb.makeMyers(BOARD, AOCL_FLAGS, blocking=blocking)
+mb.makeBpc(BOARD, AOCL_FLAGS, blocking=blocking)
