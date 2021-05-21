b4249f46e54f3744a9fb7eddea9c5183d7233a37
David Castells-Rufas <david.castells@uab.cat>
2021-05-21 17:55:01 +0200

    extract functions

--- fpga/PACS10/bpc_report.py
+++ fpga/PACS10/bpc_report.py
@@ -3,7 +3,28 @@ import sys
 sys.path.append('..')
 import report_board as rb
 
+def extractLEs(alms):
+    if (alms == '?'):
+        return '?', '?'
 
+    part = alms.split('/')
+    num = int(part[0].replace(',',''))
+    den = int(part[1].replace(',',''))
+    perc = num/den
+    totalkLEs = 2753
+    kLEs = int(totalkLEs * perc)
+    return kLEs, int(perc*100)
+
+def extractFFs(regs):
+    totalkFFs = 3732.480
+    if (regs == '?'):
+        return '?', '?'
+    num = int(regs)/1000
+    den = totalkFFs
+    perc = num/den
+    kFFs = int(totalkFFs * perc)
+    return kFFs, int(perc*100)
+ 
 
 if __name__ == "__main__":
     
@@ -14,21 +35,27 @@ if __name__ == "__main__":
 
     print('BPC resources in {}'.format(board))
     print('-------------------------------')
-    print('|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|'.format('et', 'th', 'pl', 'tl', 'LUTs', 'FFs', 'fmax'))
+    print('|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|'.format('et', 'th', 'pl', 'tl', 'kLEs', '%LEs', 'kFFs', '%FFs', 'fmax'))
 
-    for pl in [100, 150, 200, 250, 300, 350]:
+    for pl in [100, 150, 200, 250, 300]:
         for erate in [0.03, 0.05, 0.1]:
             th = int(erate*pl)
             tl = pl + 2 * th
             total = pl + tl
             if (total > (512//2)):
                 et = 1
+            if (pl > (512//2) or tl > (512//2)):
+                et = 2 
 
-            v = rb.getDesignResources(board, dsg, et, th, pl, tl)
+            v = rb.getDesignResourcesRaw(board, dsg, et, th, pl, tl)
+            
+            kLEs, pcLEs = extractLEs(v)
             regs = rb.getDesignRegisters(board, dsg, et, th, pl, tl)
+            kFFs, pcFFs = extractFFs(regs)
+            
             fmax = rb.getDesignFmax(board, dsg, et, th, pl, tl)
             #print('||{: <5}|'.format(slink, v), end='')
-            print('|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|'.format(et, th, pl, tl,v, regs, fmax))
+            print('|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|{: <5}|'.format(et, th, pl, tl, kLEs, pcLEs, kFFs, pcFFs, fmax))
 
 
 
