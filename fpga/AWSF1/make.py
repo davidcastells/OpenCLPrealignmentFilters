3aa51a3d0f42a468fb63b77f8e4ccddf2d58b25f
David Castells-Rufas <david.castells@uab.cat>
2021-07-21 19:38:33 +0200

    initial

---
+++ fpga/AWSF1/make.py
@@ -0,0 +1,20 @@
+BOARD='AWSF1'
+TIME='/usr/bin/time -f TIME=%E'
+AOCL_FLAGS=''
+
+import sys
+sys.path.append('..')
+import make_board as mb
+
+blocking=False
+
+#mb.makeSemiglobalVariants(BOARD, AOCL_FLAGS, blocking=blocking)
+#mb.makeMetaVariants(BOARD, AOCL_FLAGS, blocking=blocking)
+
+#mb.makeVariants(BOARD, AOCL_FLAGS)
+#mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[3,7,10]], lens = [150], entry_types = [1])
+#mb.makeVariants(BOARD, AOCL_FLAGS, ths= [[15]], lens = [300], entry_types = [2])
+#mb.makeLev(BOARD, AOCL_FLAGS, blocking=blocking)
+#mb.makeMyers(BOARD, AOCL_FLAGS, blocking=blocking)
+mb.makeBpc(BOARD, AOCL_FLAGS, blocking=blocking)
+
